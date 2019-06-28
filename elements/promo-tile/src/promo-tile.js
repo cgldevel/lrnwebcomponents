14931b98d512b6b163b1241aaeb7664efd7b3c5f
Charles Lavera <cgldevel@gmail.com>
2019-06-28 12:17:22 -0400

    Update promo-tile.js
    Refactored w/ CSS variables to make more flexible.

--- elements/promo-tile/src/promo-tile.js
+++ elements/promo-tile/src/promo-tile.js
@@ -1,246 +1,230 @@
-import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
-import { afterNextRender } from "@polymer/polymer/lib/utils/render-status.js";
-import { SchemaBehaviors } from "@lrnwebcomponents/schema-behaviors/schema-behaviors.js";
-import { HAXWiring } from "@lrnwebcomponents/hax-body-behaviors/lib/HAXWiring.js";
+import { html, Polymer } from "@polymer/polymer/polymer-legacy.js";
+import "@lrnwebcomponents/hax-body-behaviors/hax-body-behaviors.js";
+import "@lrnwebcomponents/schema-behaviors/schema-behaviors.js";
+import "@polymer/paper-button/paper-button.js";
+import "@polymer/iron-icon/iron-icon.js";
+import "@polymer/iron-icons/iron-icons.js";
 /**
- * `promo-tile`
- * @demo demo/index.html
- */
-class PromoTile extends SchemaBehaviors(PolymerElement) {
-  constructor() {
-    super();
-    import("@polymer/paper-button/paper-button.js");
-    import("@polymer/iron-icon/iron-icon.js");
-    import("@polymer/iron-icons/iron-icons.js");
-    afterNextRender(this, function() {
-      this.HAXWiring = new HAXWiring();
-      this.HAXWiring.setup(PromoTile.haxProperties, PromoTile.tag, this);
-    });
-  }
-  static get template() {
-    return html`
-      <style>
-        :host {
-          display: block;
-          --tile-image: "";
-          --front-title-text-shadow: #363533;
-          --title-font-size: 34px;
-          --title-font-weight: 400;
-          --back-content-font-size: 18px;
-          --back-content-font-weight: 100;
-          --font-color: #fff;
-          --hover-background-color: #e2801e;
-          --button-hover-color: #363533;
-        }
+`promo-tile`
+A LRN element
 
-        a {
-          text-decoration: none;
-        }
+* @demo demo/index.html
 
-        #container {
-          width: 100%;
-          height: auto;
-        }
+@microcopy - the mental model for this element
+ -
+ -
 
-        .back_card {
-          background-color: var(--hover-background-color);
-          height: 460px;
-          opacity: 0;
-          display: flex;
-          flex-direction: column;
-        }
+*/
+let PromoTile = Polymer({
+  _template: html`
+    <style>
+      :host {
+        display: block;
+      }
 
-        :host([hover]) #container .back_card {
-          opacity: 0.9;
-          transition: all 0.3s ease-in-out;
-        }
+      a {
+        text-decoration: var(--promo-tile-a-text-decoration, none);
+        @apply --promo-tile-a
+      }
 
-        :host([hover]) #container .front_card .front_title {
-          opacity: 0;
-          transition: all 0.3s ease-in-out;
-        }
+      #container {
+        width: var(--promo-tile-container-width, 100%);
+        height: var(--promo-tile-container-height, auto);
+        @apply --promo-tile-container
+      }
 
-        .image {
-          display: flex;
-          justify-content: center;
-          background-image: var(--tile-image);
-          background-position: top center;
-          background-repeat: no-repeat;
-          background-size: cover;
-          width: 100%;
-          height: 100%;
-        }
+      .back_card {
+        background-color: var(--promo-tile-back-card-background-color, #e2801e);
+        height: var(--promo-tile-back-card-height, 460px);
+        opacity: var(--promo-tile-back-card-opacity, 0);
+        display: var(--promo-tile-back-card-display, flex);
+        flex-direction: var(--promo-tile-back-card-flex-direction, column);
+        @apply --promo-tile-back-card
+      }
 
-        .front_title {
-          opacity: 1;
-          position: absolute;
-          display: flex;
-          align-self: flex-end;
-          padding-bottom: 25px;
-        }
+      :host([hover]) #container .back_card {
+        opacity: var(--promo-tile-container-back-card-hover-opacity, 0.9);
+        transition: var(--promo-tile-container-back-card-hover-transition, all 0.3s ease-in-out);
+        @apply --promo-tile-container-back-card-hover
+      }
 
-        .front_title h1 {
-          color: var(--font-color);
-          font-size: var(--title-font-size);
-          font-weight: var(--title-font-weight);
-          text-shadow: 1px 1px 3px var(--front-title-text-shadow);
-        }
+      :host([hover]) #container .front_card .front_title {
+        opacity: var(--promo-tile-container-front-card-front-title-hover-opacity, 0);
+        transition: var(--promo-tile-container-front-card-front-title-hover-transition, all 0.3s ease-in-out);
+        @apply --promo-tile-container-front-card-hover
+      }
 
-        .back_title {
-          opacity: 1;
-          display: flex;
-          justify-content: center;
-          padding: 20px 0 0;
-        }
+      .image {
+        display: var(--promo-tile-image-display, flex);
+        justify-content: var(--promo-tile-image-justify-content, center);
+        background-position: var(--promo-tile-image-background-position, top center);
+        background-repeat: var(--promo-tile-image-background-repeat, no-repeat);
+        background-size: var(--promo-tile-image-background-size, cover);
+        width: var(--promo-tile-image-width, 100%);
+        height: var(--promo-tile-image-height, 100%);
+        @apply --promo-tile-image
+      }
 
-        .back_title h1 {
-          color: var(--font-color);
-          font-size: var(--title-font-size);
-          font-weight: var(--title-font-weight);
-        }
+      .front_title {
+        opacity: var(--promo-tile-front-title-opacity, 1);
+        position: var(--promo-tile-front-title-position, absolute);
+        display: var(--promo-tile-front-title-display, flex);
+        align-self: var(--promo-tile-front-title-align-self, flex-end);
+        padding: var(--promo-tile-front-title-padding, 0 0 25px 0);
+        @apply --promo-tile-front-title
+      }
 
-        .back_content {
-          color: var(--font-color);
-          font-size: var(--back-content-font-size);
-          font-weight: var(--back-content-font-weight);
-          padding: 0 20px;
-        }
+      .front_title h1 {
+        color: var(--promo-tile-front-title-h1-color, #ffffff);
+        font-size: var(--promo-tile-front-title-h1-font-size, 36px);
+        font-weight: var(--promo-tile-front-title-h1-font-weight, 400);
+        text-shadow: var(--promo-tile-front-title-h1-text-shadow, 1px 1px 3px var(--promo-tile-front-title-h1-text-shadow-color, #363533));
+        @apply --promo-title-front-title-h1
+      }
 
-        paper-button#learn {
-          display: flex;
-          margin-top: 180px;
-          font-size: 16px;
-          color: var(--font-color);
-          border: solid 1px #fff;
-          border-radius: 0;
-          width: 50%;
-          margin-left: auto;
-          margin-right: auto;
-        }
+      .back_title {
+        opacity: var(--promo-tile-back-title-opacity, 1);
+        display: var(--promo-tile-back-title-display, flex);
+        justify-content: var(--promo-tile-back-title-justify-content, center);
+        padding: var(--promo-tile-back-title-padding, 20px 0 0 0);
+        @apply --promo-tile-back-title
+      }
 
-        paper-button#learn:hover,
-        paper-button#learn:focus {
-          background-color: var(--button-hover-color);
-        }
-      </style>
+      .back_title h1 {
+        color: var(--promo-tile-back-title-h1-color, #ffffff);
+        font-size: var(--promo-tile-back-title-h1-font-size, 36px);
+        font-weight: var(--promo-tile-back-title-h1-font-weight, 400);
+        @apply --promo-tile-back-title-h1
+      }
 
-      <div id="container">
-        <div class="front_card">
-          <div id="front_image" class="image" alt="[[alt]]">
-            <div class="front_title">
+      .back_content {
+        color: var(--promo-tile-back-content-font-color, #ffffff);
+        font-size: var(--promo-tile-back-content-font-size, 18px);
+        font-weight: var(--promo-tile-back-content-font-weight, 300);
+        line-height: var(--promo-tile-back-content-line-height, 1.4);
+        padding: var(--promo-title-back-content-padding, 0 20px 0 20px);
+        text-align: justify;
+        @apply --promo-tile-back-content
+      }
+
+      paper-button#learn {
+        display: var(--promo-tile-paper-button-learn-display, flex);
+        margin: var(--promo-tile-paper-button-learn-margin, 140px auto 0 auto);
+        font-size: var(--promo-tile-paper-button-learn-font-size, 18px);
+        color: var(--promo-tile-paper-button-learn-font-color, #ffffff);
+        border: var(--promo-tile-paper-button-learn-border, solid);
+        border-width: var(--promo-tile-paper-button-learn-border-width, 1px);
+        border-color: var(--promo-tile-paper-button-learn-border-color, #ffffff);
+        border-radius: var(--promo-tile-paper-button-learn-border-radius, 0);
+        width: var(--promo-tile-paper-button-learn-width, 50%);
+        @apply --promo-tile-paper-button-learn
+      }
+
+      paper-button#learn:hover,
+      paper-button#learn:focus {
+        background-color: var(--promo-tile-paper-button-learn-background-color-active, #363533);
+        @apply --promo-tile-paper-button-learn-active
+      }
+    </style>
+    <div id="container">
+      <div class="front_card">
+        <div id="front_image" class="image" alt="[[alt]]" style$="background-image:url([[image]])">
+          <div class="front_title">
+            <h1>[[title]]</h1>
+          </div>
+          <div class="back_card" id="cardBack" on-click="activateBtn">
+            <div class="back_title">
               <h1>[[title]]</h1>
             </div>
-            <div class="back_card" id="cardBack" on-click="activateBtn">
-              <div class="back_title">
-                <h1>[[title]]</h1>
-              </div>
-              <div class="back_content">
-                <slot></slot>
-              </div>
-              <div class="learn_more">
-                <a
-                  tabindex="-1"
-                  href="[[url]]"
-                  id="link"
-                  target$="[[_urlTarget(url)]]"
-                >
-                  <paper-button id="learn" no-ink
-                    >[[label]]
-                    <iron-icon icon="chevron-right"></iron-icon>
-                  </paper-button>
-                </a>
-              </div>
+            <div class="back_content">
+              <slot></slot>
+            </div>
+            <div class="learn_more">
+              <a
+                tabindex="-1"
+                href="[[url]]"
+                id="link"
+                target$="[[_urlTarget(url)]]"
+              >
+                <paper-button id="learn" no-ink
+                  >[[label]]
+                  <iron-icon icon="chevron-right"></iron-icon>
+                </paper-button>
+              </a>
             </div>
           </div>
         </div>
       </div>
-    `;
-  }
+    </div>
+  `,
 
-  static get tag() {
-    return "promo-tile";
-  }
+  is: "promo-tile",
+  behaviors: [HAXBehaviors.PropertiesBehaviors, SchemaBehaviors.Schema],
 
-  static get properties() {
-    let props = {
-      /**
-       * Image source
-       */
-      image: {
-        type: String,
-        value: "",
-        reflectToAttribute: true
-      },
-      /**
-       * Alt text for image
-       */
-      alt: {
-        type: String,
-        value: "",
-        reflectToAttribute: true
-      },
-      /**
-       * Label for button
-       */
-      label: {
-        type: String,
-        value: "",
-        reflectToAttribute: true
-      },
-      /**
-       * Title of tile
-       */
-      title: {
-        type: String,
-        value: "",
-        reflectToAttribute: true
-      },
-      /**
-       * Url for tile
-       */
-      url: {
-        type: String,
-        value: "",
-        reflectToAttribute: true
-      },
-      /**
-       * Hover state
-       */
-      hover: {
-        type: Boolean,
-        value: false,
-        reflectToAttribute: true
-      }
-    };
-    if (super.properties) {
-      props = Object.assign(props, super.properties);
+  properties: {
+    /**
+     * Image source
+     */
+    image: {
+      type: String,
+      value: "",
+      reflectToAttribute: true
+    },
+    /**
+     * Alt text for image
+     */
+    alt: {
+      type: String,
+      value: "",
+      reflectToAttribute: true
+    },
+    /**
+     * Label for button
+     */
+    label: {
+      type: String,
+      value: "",
+      reflectToAttribute: true
+    },
+    /**
+     * Title of tile
+     */
+    title: {
+      type: String,
+      value: "",
+      reflectToAttribute: true
+    },
+    /**
+     * Url for tile
+     */
+    url: {
+      type: String,
+      value: "",
+      reflectToAttribute: true
+    },
+    /**
+     * Hover state
+     */
+    hover: {
+      type: Boolean,
+      value: false,
+      reflectToAttribute: true
     }
-    return props;
-  }
-  static get observers() {
-    return ["__updateStyles(image)"];
-  }
+  },
+
+  listeners: {
+    mouseover: "__hoverIn",
+    mouseout: "__hoverOut",
+    focusin: "__hoverIn",
+    focusout: "__hoverOut"
+  },
+
   /**
    * Attached to the DOM, now fire.
    */
-  connectedCallback() {
-    super.connectedCallback();
-    afterNextRender(this, function() {
-      this.addEventListener("mouseover", this.__hoverIn.bind(this));
-      this.addEventListener("mouseout", this.__hoverOut.bind(this));
-      this.addEventListener("focusin", this.__hoverIn.bind(this));
-      this.addEventListener("focusout", this.__hoverOut.bind(this));
-    });
-  }
-  disconnectedCallback() {
-    this.removeEventListener("mouseover", this.__hoverIn.bind(this));
-    this.removeEventListener("mouseout", this.__hoverOut.bind(this));
-    this.removeEventListener("focusin", this.__hoverIn.bind(this));
-    this.removeEventListener("focusout", this.__hoverOut.bind(this));
-    super.disconnectedCallback();
-  }
-  static get haxProperties() {
-    return {
+  attached: function() {
+    // Establish hax property binding
+    let props = {
       canScale: true,
       canPosition: true,
       canEditSource: false,
@@ -326,26 +310,24 @@ class PromoTile extends SchemaBehaviors(PolymerElement) {
         advanced: []
       }
     };
-  }
-  __updateStyles(image) {
-    this.updateStyles({ "--tile-image": `url(${image})` });
-  }
+    this.setHaxProperties(props);
+  },
 
   /**
    * Internal function to check if a url is external
    */
-  _outsideLink(url) {
+  _outsideLink: function(url) {
     if (url.indexOf("http") != 0) return false;
     var loc = location.href,
       path = location.pathname,
       root = loc.substring(0, loc.indexOf(path));
     return url.indexOf(root) != 0;
-  }
+  },
 
   /**
    * If url is external, open link in new window, otherwise open link in same window.
    */
-  _urlTarget(url) {
+  _urlTarget: function(url) {
     if (url) {
       const external = this._outsideLink(url);
       if (external) {
@@ -353,23 +335,22 @@ class PromoTile extends SchemaBehaviors(PolymerElement) {
       }
     }
     return false;
-  }
+  },
 
-  activateBtn() {
+  activateBtn: function() {
     if (this.hover) {
-      const link = this.shadowRoot.querySelector("#link");
+      const link = this.$.link;
       if (window.innerWidth > 700) {
         link.click();
       }
     }
-  }
+  },
 
-  __hoverIn() {
+  __hoverIn: function() {
     this.hover = true;
-  }
-  __hoverOut() {
+  },
+  __hoverOut: function() {
     this.hover = false;
   }
-}
-window.customElements.define(PromoTile.tag, PromoTile);
+});
 export { PromoTile };
